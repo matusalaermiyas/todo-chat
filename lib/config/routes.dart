65ca0d20d67ef3bfc2fd4be30f17b4186379e12c
MatusalaErmiyas <matusala.dev@gmail.com>
2024-10-24 16:49:13 +0300

    Implement routing

---
+++ lib/config/routes.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:todo_chat/auth/signin_screen.dart';
+import 'package:todo_chat/auth/signup_screen.dart';
+import 'package:todo_chat/home/home_screen.dart';
+
+final routes = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: HomeScreen.url,
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: SigninScreen.url,
+    builder: (context, state) => const SigninScreen(),
+  ),
+  GoRoute(
+    path: SignupScreen.url,
+    builder: (context, state) => const SignupScreen(),
+  ),
+]);
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,21 +1,24 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
-
-// 🌎 Project imports:
-import 'package:todo_chat/home/home_screen.dart';
+import 'package:todo_chat/config/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+    return MaterialApp.router(
+      title: "Todo Chat",
+      routerConfig: routes,
     );
+
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //     useMaterial3: true,
+    //   ),
+    //   home: const LoginScreen(),
+    // );
   }
 }
