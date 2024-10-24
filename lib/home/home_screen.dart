05a100ffde5547db5da7d03742295362118c0189
MatusalaErmiyas <matusala.dev@gmail.com>
2024-10-24 11:48:49 +0300

    Initial commit

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("2DO-Chat"),
+      ),
+      body: const Center(
+        child: Text("Welcome!"),
+      ),
+    );
+  }
+}
