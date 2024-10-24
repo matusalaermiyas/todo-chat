cf93a906f6c028926212f687508bed75f8d4a9bf
MatusalaErmiyas <matusala.dev@gmail.com>
2024-10-24 12:26:39 +0300

    Add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_chat/firebase_options.dart';
 import 'package:todo_chat/my_app.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
