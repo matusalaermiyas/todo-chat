5fce44398763b56bdba3b6023664cc57c252a6aa
MatusalaErmiyas <matusala.dev@gmail.com>
2024-10-25 16:58:41 +0300

    Add bottom navigation bar to home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,17 +1,58 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:todo_chat/chat/chat_screen.dart';
+import 'package:todo_chat/todo/todo_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  static String url = "/";
+
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+
+  final List<Widget> _screens = const [TodoScreen(), ChatScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("2DO-Chat"),
+      drawer: Container(
+        color: Colors.white,
+        width: 300,
       ),
-      body: const Center(
-        child: Text("Welcome!"),
+      appBar: AppBar(
+        leading: const Icon(
+          LineIcons.bars,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.pink,
+        title: const Text(
+          "DoChat",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: _screens[_page],
+      floatingActionButton: const Icon(LineIcons.plusCircle),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(LineIcons.edit), label: 'TODO'),
+          BottomNavigationBarItem(
+              icon: Icon(LineIcons.facebookMessenger), label: 'Chat'),
+        ],
+        currentIndex: _page,
+        selectedItemColor: Colors.pink,
+        onTap: _onItemTapped,
       ),
     );
   }
