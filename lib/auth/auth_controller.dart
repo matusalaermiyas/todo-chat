62356035e0d5e132e98dcc0902d18b9e91403a85
MatusalaErmiyas <matusala.dev@gmail.com>
2024-10-25 16:59:31 +0300

    Implement auth controller

---
+++ lib/auth/auth_controller.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_chat/utils/logger.dart';
+
+class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  get user => _auth.currentUser;
+
+  // Login method
+  Future<UserCredential> signUp(
+      {required String email, required String password}) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      AppLogger.log("Error while signin user");
+      AppLogger.log(e);
+
+      return throw Exception(e.message);
+    }
+  }
+
+  // Register method
+  Future<UserCredential> signIn(
+      {required String email, required String password}) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      return throw Exception(e.message);
+    }
+  }
+
+  // Logout
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
+}
